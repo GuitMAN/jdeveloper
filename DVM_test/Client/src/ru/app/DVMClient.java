c5851da841c83f0a8593972cb3b33954b612e3eb
Dementyev Vladimir <sayman000111@gmail.com>
2018-02-22 14:43:25 +0300

    000 "ewqweqw"
    This reverts commit 25b636c5ad4f7f2b2930ac2250eb4755b3fdc515.

---
+++ DVM_test/Client/src/ru/app/DVMClient.java
@@ -0,0 +1,74 @@
+package ru.app;
+
+import org.apache.xmlbeans.XmlObject;
+
+
+
+
+public class DVMClient {
+
+
+     public static void main(String[] args) {
+        try {
+            /**
+            *http://<host>:<managedServerPort>/soa-infra/services/<partition-name>/<compositeProjectName>/<dvm-name>.dvm
+            */
+            String dvmLoc = "D:\\NeoFlex_MDS\\Master\\oracle_mds\\v1\\src\\apps\\EDM\\VTB24\\Dvm\\cm.Country.mappingTable.V1.dvm";
+
+            String srcColumnName = "localObjectKey";
+            String srcColumnVal = "ABH";
+            String destColumnName = "cmObjectKey";
+            String targetColumnName = "system";
+            
+            dvmNeoUtils m = new dvmNeoUtils();
+            System.out.println("Отключаем валидацию");
+            m.setisVailateDVM(false);
+            String lookupVal;
+            long startTime = 0;
+            long timeSpent ;
+            
+            startTime = System.currentTimeMillis();
+            System.out.println("Начало вывоза lookupValue... ");
+           
+           // lookupVal = m.lookupValue(dvmLoc, srcColumnName, srcColumnVal, destColumnName, "не найдено", targetColumnName,"cm.System.BQ","")
+            lookupVal = m.lookupValue(dvmLoc, srcColumnName,  srcColumnVal, destColumnName,   targetColumnName, "cm.System.BQ") ;
+            timeSpent = System.currentTimeMillis() - startTime;
+            System.out.println("Затраченное время: " + timeSpent + " миллисекунд");
+            System.out.println("Response lookupValue :->" + lookupVal);
+                
+            //Вызываем  справочник повторно         
+            
+            srcColumnVal = "ARE";
+            System.out.println("Включаем валидацию и ждем пока провалидируется справочник");
+            System.out.println("Начало вывоза lookupValue... ");
+            m.setisVailateDVM(true);
+            
+            startTime = System.currentTimeMillis();
+            lookupVal = m.lookupValue(dvmLoc, srcColumnName,  srcColumnVal, destColumnName,   targetColumnName, "cm.System.BQ") ;            timeSpent = System.currentTimeMillis() - startTime;
+            System.out.println("Затраченное время: " + timeSpent + " миллисекунд");
+            System.out.println("Response lookupValue :->" + lookupVal);
+            
+            
+            m.setisVailateDVM(false);
+            System.out.println("Снова отключаем валидацию и ждем пока провалидируется справочник");
+            
+            startTime = System.currentTimeMillis();
+            lookupVal = m.lookupValue(dvmLoc, srcColumnName,  srcColumnVal, destColumnName,   targetColumnName, "cm.System.BQ") ;            timeSpent = System.currentTimeMillis() - startTime;
+            System.out.println("Затраченное время: " + timeSpent + " миллисекунд");
+            System.out.println("Response lookupValue :->" + lookupVal);
+            
+            
+           
+            
+            startTime = System.currentTimeMillis();
+            lookupVal = m.lookupValue(dvmLoc, srcColumnName,  srcColumnVal, destColumnName,   targetColumnName, "cm.System.BQ") ;            timeSpent = System.currentTimeMillis() - startTime;
+            System.out.println("Затраченное время: " + timeSpent + " миллисекунд");
+            System.out.println("Response lookupValue :->" + lookupVal);
+       
+        } catch (Exception e) {
+            System.out.println("!!!!");
+            e.printStackTrace();
+        }
+    }
+
+}
